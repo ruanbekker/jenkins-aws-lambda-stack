207664c2db3751e53c08ec5e32c926f321a533d9
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-13 09:11:13 +0200

    Create ship_function_to_s3.sh

---
+++ bin/ship_function_to_s3.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+S3_BUCKET="${S3_BUCKET_NAME:-none}"
+S3_KEY="${S3_KEY_NAME:-none}"
+AWS_PROFILE="${AWS_PROFILE:-none}"
+PACKAGE_NAME="${LAMBDA_PACKAGE_NAME:-package.zip}"
+
+# check if deployment package exist
+if [ ! -f package.zip ]
+  then 
+    echo "deployment package does not exist"
+    exit 1
+fi
+
+# ship deployment package to s3
+aws --profile ${AWS_PROFILE} cp ${PACKAGE_NAME} s3://${S3_BUCKET}/${S3_KEY}
