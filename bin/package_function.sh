9b289f8079d5ab0bdac60394cf8b0458eb9d3548
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-13 09:03:45 +0200

    Create package_function.sh

---
+++ bin/package_function.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -ex
+
+# create a virtual environment
+venv_id=$(date +%s)
+python_path=$(which python)
+virtualenv -p ${python_path} ${venv_id}
+source ${venv_id}/bin/activate
+
+# install dependencies and place in current directory
+pip install -r requirements.txt -t .
+deactivate
+rm -rf ${venv_id}
+
+# package function with dependencies
+zip -r package.zip .
+
+# cleanup
+find ${PWD} -type d -maxdepth 1 -mindepth 1 -exec rm -rf {} \;
